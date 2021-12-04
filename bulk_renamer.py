1e6861e63d1358cade1be46782e9b601fcc86ca2
Aashish2520c <ashishchapain646@gmail.com>
2021-12-04 19:59:15 +0545

    Intial Commit

---
+++ bulk_renamer.py
@@ -0,0 +1,23 @@
+"""Bulk renamer in python"""
+# Author: Aashish Sharma
+#
+
+
+def bulk_renamer(path, extension, files_name):
+    """Function to bulk rename files"""
+    import os
+    files_count = 1
+    for file in os.listdir(path):
+        if os.path.splitext(file)[1] == f".{extension}":
+            os.rename(f"{path}{file}",
+                      f"{path}{files_name}{files_count}.{extension}")
+            files_count += 1
+
+
+if __name__ == '__main__':
+    files_path = str(input("Enter the directory path: "))
+    files_extension = str(input("Enter the file extension to rename: "))
+    renamed_name = str(input("What you want to name your files?: "))
+
+    bulk_renamer(files_path, files_extension, renamed_name)
+    print("Done! \nCheck your files...")
---
+++ search_engine.py
@@ -0,0 +1,36 @@
+# Search engine
+# Author : Aashish Sharma
+import re
+# just another mistake
+
+def search(user_search, user_list):
+    """Take search keyword and list then return the most occurred element in descending order """
+    result_list = []
+    for element in user_list:
+        try:
+            # search keyword inside element
+            # this will return list with repeated keyword
+            result = re.findall(user_search, element, re.IGNORECASE)
+
+        except:
+            print("\nPlease, Enter valid keyword!")
+            break
+        else:
+
+            # count searched keyword length
+            result_count = len(result)
+
+            # to clear previous content and to save only statement and searched keyword len
+            result.clear()
+
+            # if search result keyword is 0 why waste time to save it when not 0
+            if result_count != 0:
+                result.append(result_count)
+                result.append(element)
+                result_list.append(result)
+            else:
+                continue
+    sorted_list = sorted(result_list, key=lambda x: x[0], reverse=True)
+
+    # 'sorted_list' is list of list
+    return sorted_list
