aee4af22292606271f78ed6b4e526f9bbc21cddc
Aashish2520c <ashishchapain646@gmail.com>
2021-12-05 07:52:31 +0545

    kdfkdfjk

---
+++ facebook_login.py
@@ -0,0 +1,16 @@
+# automating browser to login facebook account
+# Author: Aashish Sharma
+# done
+from selenium import webdriver
+
+url = "https://www.facebook.com/"
+browser = webdriver.Firefox()
+
+browser.get(url)
+
+user_email = browser.find_element_by_id("email")
+user_email.send_keys("9779840221385")
+
+user_password = browser.find_element_by_id("pass")
+user_password.send_keys("71904638930")
+user_password.submit()
